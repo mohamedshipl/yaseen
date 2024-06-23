ff561829c0bce5ed34f6d674c77af73b0fd49cb7
mohamedshipl <114057118+mohamedshipl@users.noreply.github.com>
2024-06-23 20:36:55 +0300

    backend index.js change

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,40 @@
-const express=require('express');
-const cors=require('cors');
-const cookieParser=require('cookie-parser')
-require('dotenv').config();
-const connectDB=require('./config/db')
-const router=require('./routes');
-const bodyParser=require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const router = require("./routes");
+const bodyParser = require("body-parser");
 
 
-const app=express();
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
-}));
-app.use(express.json())
-app.use("/api",router)
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://yaseen-fz2m.vercel.app"); // Or '*' for any origin
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+    // Handle preflight requests directly
+    if (req.method === "OPTIONS") {
+      res.sendStatus(200); // Respond to preflight with success
+    } else {
+      next(); // Continue to other routes
+    }
+  });
+app.use(express.json());
+app.use("/api", router);
 
-const PORT=8080|| process.env.PORT
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server is running on port ${PORT}`)
-    });
-})
+
+const PORT = 8080 || process.env.PORT;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
